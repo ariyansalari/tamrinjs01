08bfb878cf9998d802e4cbef75d22f3f0738dab5
ariyan salari <ariyan4509@gmail.com>
2022-12-17 14:32:25 -0800

    tamrinjs

---
+++ index.js
@@ -0,0 +1,45 @@
+const headings=document.querySelector("#hi");
+const spans=document.querySelector(".span-click");
+const buttons=document.querySelector("button");
+const lists=document.querySelector("#alobbe").children[0].textContent;
+const saz=document.getElementsByTagName("ul");
+
+const changebutton=()=>{
+let name="ariyan"
+const names=name.split("")
+console.log(names)
+
+
+}
+changebutton();
+
+const username={
+    firstName:"ali",
+    lastName:"salari",
+     location:{
+        place:"tehran",
+        apartement:"kaj",
+        code:104,
+        floor:2,
+    },
+};
+
+console.log(username.location.code);
+
+spans.style.color="red";
+
+
+
+
+for(let i=0;i<=8;i++)
+{
+    console.log(i);
+}
+
+let array=["banana","apple","tomato","orange"]
+
+for(let wa=0;wa<=array;wa++)
+{
+    console.log()
+}
+console.log(lists)
